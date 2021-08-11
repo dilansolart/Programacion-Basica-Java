8b1b035261c796fbeff226c7655a38c74f2be179
dilansolart <dilandavid94@gmail.com>
2021-08-11 10:41:55 -0500

    Modificado Sesion 16

--- Sesion_16/src/javaConnect/Connect.java
+++ Sesion_16/src/javaConnect/Connect.java
@@ -6,11 +6,11 @@ import java.sql.SQLException;
 
 public class Connect {
     public Connection connect() {
-        String url = "jdbc:sqlite:E:/Archivos de programas/SQLiteStudio/Almacen.db";
+        String url = "jdbc:sqlite:E:/WS_D2ST/WS_UniNorte/Programacion-Basica-Java/Sesion_16/db/Almacen.db";
         Connection var1 = null;
         try {
             var1 = DriverManager.getConnection(url);
-            System.out.println("Conectado!");
+            System.out.println("Conectado a la base de datos!");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
