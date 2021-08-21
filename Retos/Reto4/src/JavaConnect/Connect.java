939826f786276df7b0208177337809681f0b26b5
dilansolardt <dilandavid94@gmail.com>
2021-08-20 21:40:15 -0500

    Reto 4 - DONE!

---
+++ Retos/Reto4/src/JavaConnect/Connect.java
@@ -0,0 +1,19 @@
+package JavaConnect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    public Connection connect() {
+        String url = "jdbc:sqlite:E:/WS_D2ST/WS_UniNorte/Programacion-Basica-Java/Retos/Reto4/db/School.db";
+        Connection var1 = null;
+        try {
+            var1 = DriverManager.getConnection(url);
+            System.out.println("Conectado a la base de Datos!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return var1;
+    }
+}
---
+++ Retos/Reto4/src/SchoolSystemController.java
@@ -0,0 +1,207 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import JavaConnect.Connect;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class SchoolSystemController extends GradingSystem {
+
+    @FXML
+    private TextField iNombre, iMateria, iNota, iGenero, cNombre, cMateria;
+    @FXML
+    private Button iGuardarBtn, cargarBtn, procesarBtn, consultarBtn, eliminarBtn;
+    @FXML
+    private TextArea textDatosCargados, outData, outAccion;
+    
+    @FXML
+    void guardarReg(MouseEvent event) {
+        Connect conexion = new Connect();
+        String nombre = iNombre.getText().toLowerCase();
+        String genero = iGenero.getText().toLowerCase();
+        String materia = iMateria.getText().toLowerCase();
+        float nota = Float.parseFloat(iNota.getText());
+        try {
+            Connection conex = conexion.connect();
+            Statement stament1 = conex.createStatement();
+            stament1.executeUpdate("insert into Students(Nombre, Nota, Genero, Materia) VALUES ("+"'"+nombre+"'"+", "+"'"+nota+"'"+", "+"'"+genero+"'"+", "+"'"+materia+"'"+");");
+            System.out.println("Estudiante Creado!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    @FXML
+    void loadData(MouseEvent event) {
+        Connect conexion = new Connect();
+        String verBD = "Select * from Students";
+        String nameStudent [] = {"armando", "nicolas", "daniel", "maria", "marcela", "alexandra"};
+        String idStudent [] = {"1", "2", "3", "4", "5", "6"};
+        String matter [] = {"historia", "literatura", "biologia"};
+        String idMatter [] = {"1", "2", "3"};
+        try {
+            Connection con2 = conexion.connect();
+            Statement stament2 = con2.createStatement();
+            ResultSet rs = stament2.executeQuery(verBD);
+            ResultSetMetaData rmd = rs.getMetaData();
+            int columnas = rmd.getColumnCount();
+            int fila = 0;
+            String estudiantes = "";
+            while(rs.next()) {
+                String nombreE = rs.getString(1).toLowerCase();
+                String generoE = rs.getString(2).toLowerCase();
+                String materiaE = rs.getString(3).toLowerCase();
+                String notaE = rs.getString(4).toLowerCase();
+                fila++;
+                for(int i = 1; i <= columnas; i++) {
+                    if (i == 1) {
+                        for (int j = 0; j < nameStudent.length; j++) {
+                            boolean flag = false;
+                            if (nombreE.equals(nameStudent[j]) || nombreE.equals(idStudent[j])) {
+                                estudiantes += String.valueOf(((float) (j + 1))) + " ";
+                                flag = true;
+                                break;
+                            } else if (j == nameStudent.length - 1 && flag == false) {
+                                estudiantes += "0.0 ";
+                            }
+                        }
+                    } else if (i == 2) {
+                        if (generoE.equals("m") || generoE == "0") {
+                            estudiantes += "0.0 ";
+                        } else if (generoE.equals("f") || generoE == "1") {
+                            estudiantes += "1.0 ";
+                        }
+                    } else if (i == 3) {
+                        for (int j = 0; j < matter.length; j++) {
+                            boolean flag = false;
+                            if (materiaE.equals(matter[j]) || materiaE.equals(idMatter[j])) {
+                                estudiantes += String.valueOf(((float) (j + 1))) + " ";
+                                flag = true;
+                                break;
+                            } else if (j == matter.length - 1 && flag == false) {
+                                estudiantes += "0.0 ";
+                            }
+                        }
+                    } else if (i == 4) {
+                        estudiantes += notaE;
+                    }
+                    // estudiantes = estudiantes + rmd.getColumnName(i) + ": " + valorColumna + "   |   " ;
+                }
+                estudiantes += "\n";
+            }
+            super.n = fila;
+            textDatosCargados.setText(estudiantes);
+            super.datos = new Float[super.n][4];
+            // Creamos una mariz con los datos de los estudiantes:
+            for (int i = 0; i < super.n; i++) {
+                for (int j = 0; j < 4; j++) {
+                    super.datos[i][j] = Float.parseFloat(estudiantes.split("\n")[i].split(" ")[j]); // cadena entrada de datos
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @FXML
+    void calcularDatos(MouseEvent event) {
+        String salidaTxt = "";
+        salidaTxt += String.format("%.2f %n%d %n", super.stat1(), super.stat2());
+        // (String.format("%.2f%n %d%n %s%n %s", super.stat1(), super.stat2(), super.stat3(), super.stat4()));
+        if (super.stat3() == "-1") {
+            salidaTxt += "<Materia>\n";
+        } else {
+            salidaTxt += String.format("%s%n", super.stat3());
+        }
+        if (super.stat4() == "-1") {
+            salidaTxt += "<Estudiante>\n";
+        } else {
+            salidaTxt += String.format("%s%n", super.stat4());
+        }
+        outData.setText(salidaTxt);
+    }
+
+    @FXML
+    void consultarData(MouseEvent event) {
+        Connect conexion = new Connect();
+        Connection con2 = conexion.connect();
+        String conNombre = cNombre.getText();
+        String conMateria = cMateria.getText();
+        String consulta = "";
+        try {
+            if(!conNombre.equals("") && conMateria.equals("")) {
+                String sql = "SELECT * FROM Students WHERE Nombre=?";
+                PreparedStatement ps = con2.prepareStatement(sql);
+                ps.setString(1, conNombre);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    consulta += rs.getString("Nombre") + ' ' + rs.getString("Genero") + ' ' + rs.getString("Materia") + ' ' + rs.getString("Nota") + "\n";
+                    outAccion.setText(consulta);
+                    while(rs.next()) {
+                        consulta += rs.getString("Nombre") + ' ' + rs.getString("Genero") + ' ' + rs.getString("Materia") + ' ' + rs.getString("Nota") + "\n";
+                        outAccion.setText(consulta);
+                    }
+                } else {
+                    outAccion.setText("No se escontraron datos");
+                }
+            } else if(conNombre.equals("") && !conMateria.equals("")) {
+                String sql = "SELECT * FROM Students WHERE Materia=?";
+                PreparedStatement ps = con2.prepareStatement(sql);
+                ps.setString(1, conMateria);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    consulta += rs.getString("Nombre") + ' ' + rs.getString("Genero") + ' ' + rs.getString("Materia") + ' ' + rs.getString("Nota") + "\n";
+                    outAccion.setText(consulta);
+                    while(rs.next()) {
+                        consulta += rs.getString("Nombre") + ' ' + rs.getString("Genero") + ' ' + rs.getString("Materia") + ' ' + rs.getString("Nota") + "\n";
+                        outAccion.setText(consulta);
+                    }
+                } else {
+                    outAccion.setText("No se escontraron datos");
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @FXML
+    void eliminarData(MouseEvent event) {
+        Connect conexion = new Connect();
+        Connection con2 = conexion.connect();
+        String conNombre = cNombre.getText();
+        String conMateria = cMateria.getText();
+        String elimina = "Datos Eliminados \n";
+        try {
+            if(!conNombre.equals("") && conMateria.equals("")) {
+                String sql = "DELETE FROM Students WHERE Nombre=?";
+                PreparedStatement ps = con2.prepareStatement(sql);
+                ps.setString(1, conNombre);
+                int rs = ps.executeUpdate();
+                if (rs > 0) {
+                    outAccion.setText(elimina);
+                } else outAccion.setText("No se escontraron datos");
+            } else if(conNombre.equals("") && !conMateria.equals("")) {
+                String sql = "DELETE FROM Students WHERE Materia=?";
+                PreparedStatement ps = con2.prepareStatement(sql);
+                ps.setString(1, conMateria);
+                int rs = ps.executeUpdate();
+                if (rs > 0) {
+                    outAccion.setText(elimina);
+                } else outAccion.setText("No se escontraron datos");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
+}
