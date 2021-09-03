634554d8ee36e73b9b7a46cced7f50426d171641
dilansolart <dilandavid94@gmail.com>
2021-09-03 12:04:14 -0500

    update reto

--- Retos/Reto4/src/JavaConnect/Connect.java
+++ Retos/Reto4/src/JavaConnect/Connect.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Connect {
     public Connection connect() {
-        String url = "jdbc:sqlite:E:/WS_D2ST/WS_UniNorte/Programacion-Basica-Java/Retos/Reto4/db/School.db";
+        String url = "jdbc:sqlite:D:/WS_D2ST/WS_UniNorte/Programacion-Basica-Java/Retos/Reto4/db/School.db";
         Connection var1 = null;
         try {
             var1 = DriverManager.getConnection(url);
